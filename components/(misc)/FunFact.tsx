b92c348673b96e62994bd0d90e03d6f52f5608c8
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-08-10 19:07:59 +0530

    feat: create fun fact component

---
+++ components/(misc)/FunFact.tsx
@@ -0,0 +1,28 @@
+"use client";
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import { useHasMounted } from '@/hooks/use-has-mounted';
+import { getRandomFact } from '@/lib/misc';
+
+const FunFact = () => {
+  const [fact, setFact] = useState("");
+  const hasMounted = useHasMounted();
+
+  useEffect(() => {
+    setFact(getRandomFact());
+
+    const intervalId = setInterval(() => {
+        setFact(getRandomFact());
+    }, 10000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  if (!hasMounted){
+    return null;
+  }
+
+  return fact ? <span>fun fact? {fact}</span> : null;
+}
+
+export default FunFact
