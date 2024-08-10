5fda232686c5d9de606a00e69fdf30ebaa71680b
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-08-10 19:06:45 +0530

    feat: make the header templatable using props

--- components/(layout)/Header.tsx
+++ components/(layout)/Header.tsx
@@ -2,11 +2,16 @@ import React from 'react'
 import Link from 'next/link'
 import { ThemeToggle } from '../(theme)/ThemeSwitcher'
 
-const Header = () => {
+interface HeaderProps{
+  nick: string,
+  highlight: string
+}
+
+const Header = ({ nick, highlight }: HeaderProps) => {
   return (
     <header className="flex justify-between items-center my-4">
         <Link href="/" >
-            <h1 className='text-lg font-medium'>rudra<span className='text-purple-500 text-opacity-90'>xd</span></h1>
+            <h1 className='text-lg font-medium'>{nick}<span className='text-purple-500 text-opacity-90'>{highlight}</span></h1>
         </Link>
         <ThemeToggle />
     </header>
