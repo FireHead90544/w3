dd86def0223a88f5a6e62ee55ac742f5171261da
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-08-11 15:18:19 +0530

    chore: refactor indentation

--- app/page.tsx
+++ app/page.tsx
@@ -9,30 +9,30 @@ import FunFact from "@/components/(misc)/FunFact";
 export default function Home() {
   return (
 		<div className="flex flex-col space-y-6 my-4 w-full">
-      <ProfileContainer
-        name="rudransh joshi"
-        role="foss developer"
-        work="building llm apps"
-        org="undisclosed"
-      />
+			<ProfileContainer
+				name="rudransh joshi"
+				role="foss developer"
+				work="building llm apps"
+				org="undisclosed"
+			/>
 
 			<AboutContainer>
 				<span>
 					hi, {"i'm"} rudransh. a software engineer majoring in {"ai&ml"}.
-          my field of research and interest lies in machine learning (more specifically, deep learning).
-          neural networks, llms & calculus fascinates me a lot. i love contributing to open source projects.
+					my field of research and interest lies in machine learning (more specifically, deep learning).
+					neural networks, llms & calculus fascinates me a lot. i love contributing to open source projects.
 				</span>
 				<span>
 					i like reading stuffs & people, listening to music.
-          and builds cool stuff. <FunFact />
+          			and builds cool stuff. <FunFact />
 				</span>
 			</AboutContainer>
 
 			<LinksContainer>
-        <InternalLink href={"https://rudra.is-a.dev/static/resume.pdf"} text="resume" />
+        		<InternalLink href={"https://rudra.is-a.dev/static/resume.pdf"} text="resume" />
 				<InternalLink href={"https://rudra.is-a.dev/portfolio"} text="work" />
 				<InternalLink href={"https://rudra.is-a.dev/blog"} text="blog" />
-      </LinksContainer>
+      		</LinksContainer>
 		</div>
 	);
 }
