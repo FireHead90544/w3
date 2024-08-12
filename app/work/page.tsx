989ae3779134e79f614060d840cdd06e37f8c2ba
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-08-12 21:59:25 +0530

    chore: add dummy data

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,13 +1,44 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import InternalLink from "@/components/(misc)/InternalLink";
 
 export default function Home() {
 	return (
-		<div className="flex flex-col space-y-4 my-4 w-full items-center justify-center">
-			<span className="font-thin text-3xl">Work in Progress</span>
-            <Link href={"https://rudra.is-a.dev/"}>
-                <Button variant={"outline"}>cd ~/rudra.is-a.dev</Button>
-            </Link>
+		<div className="flex flex-col space-y-6 my-4 w-full">
+			<div className="flex flex-col">
+				<h1 className="text-3xl font-light">work</h1>
+				<span>some of my most notable works and projects</span>
+			</div>
+			<div className="flex flex-col space-y-3">
+				<div className="flex flex-col">
+					<div className="flex justify-between text-xl">
+                        <div className="space-x-2">
+                            <span>1.</span>
+                            <span>how-cli</span>
+                        </div>
+                        <div className="space-x-2">
+                            <InternalLink href="/" text="source" />
+                            <InternalLink href="/" text="deployment" />
+                        </div>
+					</div>
+					<span>context aware ai-based cli application built using langchain + typer to help you with command line and shell.</span>
+				</div>
+			</div>
+            <div className="flex flex-col space-y-3">
+				<div className="flex flex-col">
+					<div className="flex justify-between text-xl">
+                        <div className="space-x-2">
+                            <span>2.</span>
+                            <span>retro rings</span>
+                        </div>
+                        <div className="space-x-2">
+                            <InternalLink href="/" text="source" />
+                            <InternalLink href="/" text="deployment" />
+                        </div>
+					</div>
+					<span>an 8-bit rpg-themed application written w/ next.js, react, tailwindcss, chart.js, fastapi and pytorch designed to predict outcomes for the 2024 paris olympics for codedex summer hackathon 2024.</span>
+				</div>
+			</div>
 		</div>
 	);
 }
