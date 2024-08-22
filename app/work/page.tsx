4da358d24684c72db8230855fa751982c6b52a0a
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-08-23 02:16:16 +0530

    feat: styled works

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -13,30 +13,30 @@ export default function Home() {
 				<div className="flex flex-col">
 					<div className="flex justify-between text-xl">
                         <div className="space-x-2">
-                            <span>1.</span>
-                            <span>how-cli</span>
+                            <span className="font-medium">1.</span>
+                            <span className="font-medium">how-cli</span>
                         </div>
-                        <div className="space-x-2">
+                        <div className="space-x-2 text-sm sm:text-lg">
                             <InternalLink href="/" text="source" />
                             <InternalLink href="/" text="deployment" />
                         </div>
 					</div>
-					<span>context aware ai-based cli application built using langchain + typer to help you with command line and shell.</span>
+					<span className="font-extralight">context aware ai-based cli application built using langchain + typer to help you with command line and shell.</span>
 				</div>
 			</div>
             <div className="flex flex-col space-y-3">
 				<div className="flex flex-col">
 					<div className="flex justify-between text-xl">
                         <div className="space-x-2">
-                            <span>2.</span>
-                            <span>retro rings</span>
+                            <span className="font-medium">2.</span>
+                            <span className="font-medium">retro rings</span>
                         </div>
-                        <div className="space-x-2">
+                        <div className="space-x-2 text-sm sm:text-lg">
                             <InternalLink href="/" text="source" />
                             <InternalLink href="/" text="deployment" />
                         </div>
 					</div>
-					<span>an 8-bit rpg-themed application written w/ next.js, react, tailwindcss, chart.js, fastapi and pytorch designed to predict outcomes for the 2024 paris olympics for codedex summer hackathon 2024.</span>
+					<span className="font-extralight">an 8-bit rpg-themed application written w/ next.js, react, tailwindcss, chart.js, fastapi and pytorch designed to predict outcomes for the 2024 paris olympics for codedex summer hackathon 2024.</span>
 				</div>
 			</div>
 		</div>
