befebd9915f8d9cf2b0bc71f46d9ddb2802d1813
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-08-28 02:58:34 +0530

    feat: create bare minimum working blog component (todo: style)

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,9 +1,18 @@
+import { notFound } from "next/navigation";
+import { getBlogPosts } from "@/lib/blog";
+import CustomMDX from "@/components/(layout)/CustomMDX";
+
 export default function Post({ params }: { params: { slug: string } }) {
+	const post = getBlogPosts().find((post) => post.slug === params.slug);
+	if (!post) {
+		return notFound();
+	}
+
 	return (
 		<div className="flex flex-col space-y-6 my-4 w-full">
 			<div className="flex flex-col">
-				<h1 className="text-3xl font-light">{params.slug}</h1>
-				<span>post content here</span>
+				<h1 className="text-3xl font-light">{post.metadata.title.toLowerCase()}</h1>
+				<CustomMDX source={post.content} />
 			</div>
 		</div>
 	);
