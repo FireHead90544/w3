2a155993221be11a2366517dc9a5b414182c4794
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-08-30 03:19:17 +0530

    fix: remove icon if internal url

--- components/(misc)/InternalLink.tsx
+++ components/(misc)/InternalLink.tsx
@@ -3,15 +3,16 @@ import Link, { LinkProps } from 'next/link'
 import { RxArrowTopRight as ArrowTopRight } from "react-icons/rx";
 
 interface InternalLinkProps extends LinkProps {
-  text: string
-  target?: "_blank" | "_self" | "_parent" | "_top"
+  text: string | React.ReactNode,
+  target?: "_blank" | "_self" | "_parent" | "_top",
+  rel?: string
 }
 
 const InternalLink = ({ text, ...props }: InternalLinkProps) => {
   return (
     <Link className="inline-flex w-fit items-center text-foreground hover:text-muted-foreground duration-200" {...props}>
       <span className="border-b-2">{text}</span>
-      <ArrowTopRight className="relative -top-1 scale-75" />
+      {props.target==="_blank" && <ArrowTopRight className="relative -top-1 scale-75" />}
     </Link>
   )
 }
