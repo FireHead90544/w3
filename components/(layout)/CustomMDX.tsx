e31ec5d9ac4dc6dbe8c94854616acc34a4fc704e
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-08-30 03:20:03 +0530

    feat: redesign & refactoring

--- components/(layout)/CustomMDX.tsx
+++ components/(layout)/CustomMDX.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
-import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote/rsc'
+import { MDXRemote } from 'next-mdx-remote/rsc'
 import Code from '@/components/(mdx)/Code'
+import RoundedImage from '@/components/(mdx)/RoundedImage'
+import CustomLink from '@/components/(mdx)/CustomLink'
+import createHeading from '@/components/(mdx)/Heading'
 
 const components = {
+  h1: createHeading(1),
+  h2: createHeading(2),
+  h3: createHeading(3),
+  h4: createHeading(4),
+  h5: createHeading(5),
+  h6: createHeading(6),
+  img: RoundedImage,
+  a: CustomLink,
   pre: Code
 }
 
-const CustomMDX = (props: MDXRemoteProps) => {
+interface MDXProps {
+  source: string,
+  components?: any
+}
+
+const CustomMDX = (props: MDXProps) => {
   return (
     <MDXRemote {...props} components={{ ...components, ...(props.components || {}) }} />
   )
