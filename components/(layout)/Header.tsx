22ffbe411289b696328a0680306640abf346c7d8
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-09-10 23:09:14 +0530

    fix: change h1 to span for site title

--- components/(layout)/Header.tsx
+++ components/(layout)/Header.tsx
@@ -11,7 +11,7 @@ const Header = ({ nick, highlight }: HeaderProps) => {
   return (
     <header className="flex justify-between items-center my-4">
         <Link href="/" >
-            <h1 className='text-lg font-medium'>{nick}<span className='text-purple-500 text-opacity-90'>{highlight}</span></h1>
+            <span className='text-lg font-medium'>{nick}<span className='text-purple-500 text-opacity-90'>{highlight}</span></span>
         </Link>
         <ThemeToggle />
     </header>
